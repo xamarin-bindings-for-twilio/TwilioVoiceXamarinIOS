d6cb5441cd18cf99f4a55c04eacdb8a1ed9dfc39
Denis Kornev <denis@boostlingo.com>
2020-07-08 17:04:30 +0700

    twilio voice sdk updated to v5.4.1

--- src/bind.sh
+++ src/bind.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sharpie bind -o bindings -namespace=Twilio.Voice.iOS -sdk iphoneos13.2 -framework Carthage/Build/iOS/TwilioVoice.framework
+sharpie bind -o bindings -namespace=Twilio.Voice.iOS -sdk iphoneos13.5 -framework Carthage/Build/iOS/TwilioVoice.framework
